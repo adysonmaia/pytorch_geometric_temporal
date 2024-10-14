5c3abb29ad6fa2def0ec04a6ad6a6e30e4d09290
Adyson Maia <adyson.maia@gmail.com>
2024-10-14 10:28:34 +0200

    fix: fixed 'torch_geometric.utils' import in tsagcn.py

--- torch_geometric_temporal/nn/attention/tsagcn.py
+++ torch_geometric_temporal/nn/attention/tsagcn.py
@@ -1,10 +1,11 @@
 import math
-import torch
+
 import numpy as np
+import torch
 import torch.nn as nn
-from torch.autograd import Variable
-from torch_geometric.utils.to_dense_adj import to_dense_adj
 import torch.nn.functional as F
+from torch.autograd import Variable
+from torch_geometric.utils import to_dense_adj
 
 
 class GraphAAGCN:
